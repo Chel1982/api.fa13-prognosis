444068bcf5a6f770a75a4ff4b81af37782bed356
chel1982 <chel@activist.com>
2020-12-09 14:48:50 +0200

    add prefix v1

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('register', [PassportAuthController::class, 'register']);
-Route::post('login', [PassportAuthController::class, 'login']);
+Route::group(['prefix' => '/v1'], function() {
+    Route::post('register', [PassportAuthController::class, 'register']);
+    Route::post('login', [PassportAuthController::class, 'login']);
+});
