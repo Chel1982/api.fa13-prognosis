767b31721c85dbf330ae66d465e8be7a91f56ccd
chel1982 <chel@activist.com>
2020-12-14 13:08:28 +0200

    add swagger in Controller

--- app/Http/Controllers/Api/V1/PassportAuthController.php
+++ app/Http/Controllers/Api/V1/PassportAuthController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\PassportAuthController\LoginRequest;
 use App\Http\Requests\Api\V1\PassportAuthController\RegisterRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 
 /**
  * Class PassportAuthController
@@ -15,6 +16,54 @@ use Illuminate\Support\Facades\Auth;
  */
 class PassportAuthController extends Controller
 {
+    /**
+     * @OA\Post(
+     *  path="/api/v1/register",
+     *  description="Производится регистрация пользователя",
+     *  tags={"Аутентификация пользователя"},
+     *  operationId="registerUser",
+     *  @OA\Parameter(
+     *      name="name",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *  @OA\Parameter(
+     *      name="email",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *   @OA\Parameter(
+     *      name="password",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *      @OA\Parameter(
+     *      name="password_confirmation",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     * )
+     */
+
     /**
      * @param  RegisterRequest  $request
      *
@@ -34,11 +83,43 @@ class PassportAuthController extends Controller
     }
 
     /**
-     * @param  Request  $request
+     * @OA\Post(
+     *  path="/api/v1/login",
+     *  description="Производится вход в систему пользователя",
+     *  tags={"Аутентификация пользователя"},
+     *  operationId="loginUser",
+     *  @OA\Parameter(
+     *      name="email",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *   @OA\Parameter(
+     *      name="password",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     * )
+     */
+
+    /**
+     * @param  LoginRequest  $request
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
         $data = [
             'email' => $request->email,
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,8 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @OA\Info(title="fa13-prognosis API", version="0.1")
+     */
 }
