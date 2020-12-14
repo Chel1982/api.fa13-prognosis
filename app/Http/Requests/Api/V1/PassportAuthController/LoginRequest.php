a20d9173ed56a1dc9503cdb4e73ddee8a3160be1
chel1982 <chel@activist.com>
2020-12-14 11:44:53 +0200

    add LoginRequest.php

---
+++ app/Http/Requests/Api/V1/PassportAuthController/LoginRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\PassportAuthController;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|string|email|unique:users,email|max:255',
+        ];
+    }
+}
