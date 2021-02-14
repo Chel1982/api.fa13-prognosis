7101141213aad67219e235b27625730b6ebbf971
chel1982 <chel@activist.com>
2021-02-15 00:01:54 +0200

    add seed to tournament table

---
+++ database/seeders/TournamentSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TournamentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tournaments')->insert([
+            'id' => 1,
+            'name' => 'Общий турнир',
+            'status' => 'all'
+        ]);
+    }
+}
