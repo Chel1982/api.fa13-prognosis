b23389b919c475a9a23a860bcda2bf103d108410
chel1982 <chel@activist.com>
2021-02-17 20:56:51 +0200

    add send emails

---
+++ app/Console/Commands/SendEmails.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\UserFa13Email;
+use Illuminate\Console\Command;
+
+class SendEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:sendEmails';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $emails = UserFa13Email::all();
+
+        foreach ($emails as $email) {
+            sleep(rand(3,7));
+            if (mail(
+                $email->email,
+                'Новый проект от комьюнити fa13',
+                'Доброго времени суток, уважаемый менеджер ' . $email->name
+                . '. Хочу вам показать проект от сообщества fa13, в котором вы сможете
+                обсуждать матчи всех чемпионатов без цензуры(реализованы чаты на все турниры
+                и общий чат), читать все пресс-конференции(время обновления с 4 утра до 22 вечера).
+                Поскольку проект является не коммерческим, разработка проекта ведется
+                мной в свободное от работы и досуга время.
+                Прошу вас пожаловать на сайт http://fa13-prognosis.com.ua/ .
+                С уважнием, Эндрю',
+                'From: admin@fa13-prognosis.com.ua'))
+            {
+                echo $email->email . PHP_EOL;
+            } else {
+                echo "some error happen";
+            }
+        }
+    }
+}
